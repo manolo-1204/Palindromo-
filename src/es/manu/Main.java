9ea71f26aff64520dd82f583a5f707cf0fa5a7c2
Manuel José Tornero Fuster <manueljosetornero@hotmail.com>
2022-03-22 12:44:08 +0100

    finiquitaou

--- src/es/manu/Main.java
+++ src/es/manu/Main.java
@@ -5,15 +5,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String textico, invertido = "";
+        String textico, invertido = "", texticoSinEspacios = "";
         Scanner entrada = new Scanner(System.in);
-        boolean palindromo = false;
+        boolean palindromo = true;
 
-        System.out.print("Dame un textico ");
+        System.out.print("Dame un textico: ");
         textico = entrada.nextLine();
 
         for (int i = 0; i < textico.length(); i++) {
-            invertido.charAt(invertido.length() - 1 - i) = textico.charAt(i);
+            if (textico.charAt(i) != ' ') {
+                texticoSinEspacios += textico.charAt(i);
+            }
+        }
+
+        for (int i = 0; i < texticoSinEspacios.length() && palindromo; i++) {
+            if (texticoSinEspacios.toLowerCase().charAt(i) != texticoSinEspacios.toLowerCase().charAt(texticoSinEspacios.length() - i - 1)) {
+                palindromo = false;
+            }
+        }
+
+        /*for (int i = textico.length() - 1; i >= 0 ; i--) {
+            invertido += textico.charAt(i);
+        }*/
+
+        if (palindromo) {
+            System.out.println("Es palindromo");
+        } else {
+            System.out.println("No es palindromo");
         }
     }
 }
